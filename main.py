d93ff4e2ba8ab2c6194411da4ad276979f759bc7
Fabian <fool0003@hotmail.com>
2016-02-01 13:46:35 +0100

    Basic layout

---
+++ main.py
@@ -0,0 +1,26 @@
+# Main file for argon
+
+# Import classes and functions (In that order)
+# Config
+# Atom storage
+# Init function
+# Atom update (Which itself includes pos+vel update / BC)
+# Calculate temperature
+
+
+# Initialize position + velocity
+
+# Main loop
+i=0;
+for i in range(1000):
+    # Update position
+    
+    # Calculate temperature
+    
+    # Increment
+    i += 1;
+    
+# Plot temperature & positions
+
+# Show program end
+print("Program ended at i =", i);
