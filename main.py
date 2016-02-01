c11af9b664221043237190798ea70146b3fe1b54
mgarming <mgarming@outlook.com>
2016-02-01 17:14:21 +0100

    Compute tempertature

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 # Import classes and functions (In that order)
 import config
+import numpy as np
+import matplotlib.pyplot as plt
 from particles import Particles
 from initSimulation import initSimulation
+from ComputeTemp import ComputeTemp
 # Init function
 # Atom update (Which itself includes pos+vel update / BC)
 # Calculate temperature
@@ -13,15 +16,15 @@ from initSimulation import initSimulation
 particles = Particles(config.nParticles);
 initSimulation(particles);
 # Main loop
+Temp=np.zeros(config.iterations)
 for i in range(config.iterations):
     # Update position
     
     # Calculate temperature
-    
-    # Increment
-    i=i;
+    Temp[i]=ComputeTemp(particles)
     
 # Plot temperature & positions
 
 # Show program end
 print("Program ended at i =", i);
+plt.plot(Temp)
