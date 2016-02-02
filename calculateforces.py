e66956ad5b863ec19d7c35991683f9baddfdbffb
Fabian <fool0003@hotmail.com>
2016-02-02 16:18:45 +0100

    Improved speed

--- calculateforces.py
+++ calculateforces.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+# calculateForce
 
 import numpy as np
 import config as c
-import scipy.spatial.distance as sc
+#import scipy.spatial.distance as sc
 
 #calculate cell offset
 def cellOffset(current, other):
@@ -11,11 +11,12 @@ def cellOffset(current, other):
 #calculate forces
 def calculateForces(particles):
     particles.forces=np.zeros(shape=(c.nParticles,3), dtype="float64")
-    for p1 in range(c.nParticles):
-        for p2 in range(p1):
-            cell_offset = cellOffset(particles.positions[p1,:], particles.positions[p2,:]) ;
-            new_p2_position = particles.positions[p2,:]+cell_offset*c.lCalc;
-            r = sc.euclidean(particles.positions[p1,:],new_p2_position);
-            force = 24 * c.epsilon / c.sigma * (particles.positions[p1,:]-new_p2_position) * (2*(c.sigma/r)**14-(c.sigma/r)**8);
-            particles.forces[p1,:] += force;
-            particles.forces[p2,:] += -force;
+    for p1 in range(1,c.nParticles):
+        #for p2 in range(p1):
+        cell_offset = cellOffset(particles.positions[p1,:], particles.positions[range(p1),:]) ;
+        new_p2_position = particles.positions[range(p1),:] + cell_offset*c.lCalc;
+        r =  np.sum( (particles.positions[p1,:]-new_p2_position)**2, axis=1) 
+        force = np.multiply(24 * c.epsilon / c.sigma * (particles.positions[p1,:]-new_p2_position), 
+                            (2*(c.sigma/r)**7-(c.sigma/r)**4)[np.newaxis].T)
+        particles.forces[p1,:] += sum(force);
+        particles.forces[range(p1),:] += -force;
