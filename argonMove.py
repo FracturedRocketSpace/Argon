b59f1ba072db36e333725162b944fc50f2274a86
Fabian <fool0003@hotmail.com>
2016-02-02 10:35:11 +0100

    speed Fix

--- argonMove.py
+++ argonMove.py
@@ -13,8 +13,10 @@ def argonMove(particles):
         particles.positions[p1,:] += particles.velocities[p1,:] * c.dt + 0.5 / c.mass*particles.forces[p1,:] * c.dt ** 2
         keepParticlesInCell(particles);
         particles.velocities[p1,:] += 0.5/c.mass*particles.forces[p1,:] * c.dt
-        calculateForces(particles)
-        particles.velocities[p1,:] += 0.5/c.mass*particles.forces[p1,:] * c.dt
+        
+    calculateForces(particles)
+    for p1 in range(c.nParticles):
+        particles.velocities[p1,:] += 0.5/c.mass*particles.forces[p1,:] * c.dt   
 
 
     
