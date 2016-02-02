434be9b46e3a940650bfcfdea916123450638d33
Luka Bavdaz <lukabavdaz@gmail.com>
2016-02-02 09:40:44 +0100

    fixed calculate forces
    typfout

--- calculateforces.py
+++ calculateforces.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import config as c
-import particles
 import scipy.spatial.distance as sc
 
 #calculate cell offset
@@ -11,11 +10,11 @@ def cellOffset(current,other):
 
 #calculate forces
 def calculateForces(atoms):
-    for p1 in range(0,c.particles):
+    for p1 in range(0,c.nParticles):
         for p2 in range(0,p1):
             cell_offset = cellOffset(p1,p2);
             new_p2_position = atoms.positions[p2,:]+cell_offset*c.lCalc;
             r = sc.euclidean(atoms.positions[p1,:],new_p2_position);
-            force = 24*c.epsilon/c.sigma*(atoms.particles[p1,:]-new_p2_position)*(2*(c.sigma/r)**14-(c.sigma/r)**8);
+            force = 24*c.epsilon/c.sigma*(atoms.positions[p1,:]-new_p2_position)*(2*(c.sigma/r)**14-(c.sigma/r)**8);
             atoms.forces[p1,:] += force;
             atoms.forces[p2,:] += -force;
