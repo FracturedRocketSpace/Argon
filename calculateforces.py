3bc803ed3c635ef1096120bd3b26f440c796c2b4
Luka Bavdaz <lukabavdaz@gmail.com>
2016-02-02 09:55:02 +0100

    updated naming
    langere regels maar wel cosistent met de rest nu

--- calculateforces.py
+++ calculateforces.py
@@ -9,12 +9,12 @@ def cellOffset(current,other):
     return np.around((current-other)/c.lCalc);
 
 #calculate forces
-def calculateForces(atoms):
+def calculateForces(particles):
     for p1 in range(0,c.nParticles):
         for p2 in range(0,p1):
             cell_offset = cellOffset(p1,p2);
-            new_p2_position = atoms.positions[p2,:]+cell_offset*c.lCalc;
-            r = sc.euclidean(atoms.positions[p1,:],new_p2_position);
-            force = 24*c.epsilon/c.sigma*(atoms.positions[p1,:]-new_p2_position)*(2*(c.sigma/r)**14-(c.sigma/r)**8);
-            atoms.forces[p1,:] += force;
-            atoms.forces[p2,:] += -force;
+            new_p2_position = particles.positions[p2,:]+cell_offset*c.lCalc;
+            r = sc.euclidean(particles.positions[p1,:],new_p2_position);
+            force = 24*c.epsilon/c.sigma*(particles.positions[p1,:]-new_p2_position)*(2*(c.sigma/r)**14-(c.sigma/r)**8);
+            particles.forces[p1,:] += force;
+            particles.forces[p2,:] += -force;
