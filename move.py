cfb9d0164ad4ad342736cfe742d30be16b297776
Luka Bavdaz <lukabavdaz@gmail.com>
2016-02-02 10:07:09 +0100

    updated move
    renamed ArgonMove -> move
    added periodic boundary conditions

--- move.py
+++ move.py
@@ -4,13 +4,18 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+import numpy as np
 import config as c
 from calculateforces import calculateForces
+
+def keepParticlesInCell(particles):
+    for p in range(c.nParticles):
+        particles.positions[p,:] = np.remainder(particles.positions[p,:], c.lCalc);
 
 def move(particles):
     for p1 in range(c.nParticles):
         particles.positions[p1,:]+=particles.velocities[p1,:]*c.dt+0.5/c.mass*particles.force[p1,:]*c.dt**2
+        keepParticlesInCell(particles);
         particles.velocities[p1,:]+=0.5/c.mass*particles.force[p1,:]*c.dt
         calculateForces(particles)
         particles.velocities[p1,:]+=0.5/c.mass*particles.force[p1,:]*c.dt
