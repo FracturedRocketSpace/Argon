a0691d3839fe6d086279118365c87cda3617c9ee
Fabian <fool0003@hotmail.com>
2016-02-03 14:52:44 +0100

    Added a "histogram"

--- plotResults.py
+++ plotResults.py
@@ -4,7 +4,31 @@ import config
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from numba import jit
 
+@jit(nopython=True)
+def corrHist(positions):
+    g = np.zeros(config.histSteps);
+    for p1 in range(1,config.nParticles):
+        for p2 in range(p1):
+            X = positions[p2,0] - positions[p1,0];
+            Y = positions[p2,1] - positions[p1,1];
+            Z = positions[p2,2] - positions[p1,2];
+            
+            X -= np.rint(X/config.lCalc) * config.lCalc;
+            Y -= np.rint(Y/config.lCalc) * config.lCalc;
+            Z -= np.rint(Z/config.lCalc) * config.lCalc;            
+            
+            distance = np.sqrt(X*X + Y*Y + Z*Z);
+            for i in range(config.histSteps):
+                if( (config.histRange/config.histSteps) * i < 
+                    distance < (config.histRange/config.histSteps) * (i+1) ):
+                        g[i] += 1 / ( 4 * np.pi * ((config.histRange/config.histSteps*i)**2) * (config.histRange/config.histSteps) );
+                        break;    
+                    
+    g = g * 2 * (config.lCalc**3) / (config.nParticles*(config.nParticles-1));        
+    return g, np.linspace( (config.histRange/config.histSteps), config.histRange, config.histSteps )
+            
 def plotResults(particles, temp, eK, eP):
     plt.figure(1)
     plt.title('Temperature')
@@ -20,6 +44,16 @@ def plotResults(particles, temp, eK, eP):
     plt.plot( np.linspace(0, config.dt*config.iterations, config.iterations), eP, label="eP")
     plt.plot( np.linspace(0, config.dt*config.iterations, config.iterations), eP+eK, label="eK+eP")
     plt.legend()
+    
+    plt.figure(3)
+    plt.title('Correlation')
+    plt.xlabel('Distance')
+    plt.ylabel('g(r)')
+    g, bins = corrHist(particles.positions)
+    #plt.hist(g, config.histSteps, normed=1, facecolor='green', alpha=0.75)
+    plt.plot(bins, g, 'r--', linewidth=1)
+    
+    
     
     plt.show();
     
---
+++ resonanceConfig.py
@@ -0,0 +1,22 @@
+# Config file 
+
+import numpy as np
+
+animation = True;
+nParticles = 4 * (5**3); # = 4*n**3
+iterations = 500; # Number of iterations
+epsilon = 1; # Strengt of potential energy in Joule 
+sigma = 1 ; # 0 point of potential in meters
+kB = 1; # Boltzmann constant in Joule per Kelvin
+mass = 1; # Mass of argon in kg
+tInitial = 0; # Initial temperatyre Kelvin
+tRescale = 1; # Boundary temperature for solid walls in Kelvin
+rescaleIter = 50;
+stopRescaleIter = 2000;
+
+dt = 0.004; # Time step (seconds)
+lCalc =  (nParticles/4)**(1/3)*2**(2/3);
+a=np.sqrt(kB*tInitial/mass); # Used for Maxwell distribution
+
+histRange = np.sqrt(2*lCalc*lCalc) *3/4;
+histSteps = 150;
