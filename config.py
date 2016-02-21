86cd7a236145d5a021c094270f451ad65773e6ac
svandersar <stefanvandersar10@gmail.com>
2016-02-21 10:35:54 +0100

    Dichtheid instellen mogelijk
    Twee veranderingen in config aangebracht. Dit is waarschijnlijk niet de
    mooiste manier, maar het werkt wel ;-)

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ animation = False;
 animationIter = 10;
 animationRotation = 0.50;
 
-nParticles = 4 * (5**3); # = 4*n**3
-iterations = 2000; # Number of iterations
+nParticles = 4 * (6**3); # = 4*n**3
+iterations = 100; # Number of iterations
 epsilon = 1; # Strengt of potential energy in Joule 
 sigma = 1 ; # 0 point of potential in meters
 kB = 1; # Boltzmann constant in Joule per Kelvin
@@ -18,8 +18,10 @@ rescaleIter = 20;
 stopRescaleIter = 1000;
 oscLength=np.array([50, 100, 150, 200, 250, 300, 500]) #Length of wiggles in iterations; used for error calculation
 
+rho=0.65; # Choose density in reduced units
+
 dt = 0.004; # Time step (seconds)
-lCalc =  1.05*(nParticles/4)**(1/3)*2**(2/3);
+lCalc =  (1/rho)**(1/3)*(nParticles/4)**(1/3)*2**(2/3);
 a=np.sqrt(kB*tInitial/mass); # Used for Maxwell distribution
 
 histRange = np.sqrt(2*lCalc*lCalc) *3/4;
