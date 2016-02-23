a52da9404fc3150be9e58d80ce6adbedcb72b174
Fabian <fool0003@hotmail.com>
2016-02-23 21:38:00 +0100

    Made a plotter
    Figures can be used in report.
    Also added more variables to the dump

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ sigma = 1 ; # 0 point of potential in meters
 kB = 1; # Boltzmann constant in Joule per Kelvin
 mass = 1; # Mass of argon in kg
 tInitial = 0.8; # Initial temperature Kelvin
-tRescale = 0.8; # Boundary temperature for solid walls in Kelvin
-rescaleIter = 20;
+tRescale = tInitial; # Boundary temperature for solid walls in Kelvin
+rescaleIter = 10;
 stopRescaleIter = 1000;
 oscLength=np.array([50, 100, 150, 200, 250, 300, 500]) #Length of wiggles in iterations; used for error calculation
 
@@ -24,5 +24,5 @@ dt = 0.004; # Time step (seconds)
 lCalc =  (1/rho)**(1/3)*(nParticles/4)**(1/3)*2**(2/3);
 a=np.sqrt(kB*tInitial/mass); # Used for Maxwell distribution
 
-histRange = np.sqrt(2*lCalc*lCalc) *3/4;
+histRange = np.sqrt(2)*(lCalc)*3/5
 histSteps = 150;
