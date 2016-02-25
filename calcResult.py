5608e77f46b7b5b00d4c85c1cd9c56f95b798584
Luka Bavdaz <lukabavdaz@gmail.com>
2016-02-25 16:59:09 +0100

    Better bootstrap
    The samples are divided into blocks. Half of the blocks are randomly
    selected and the correct value is calculated for the unification of
    those blocks.

--- calcResult.py
+++ calcResult.py
@@ -12,16 +12,16 @@ def ComputeCv(eK):
     eKvar = np.var(eK);
     
     return 3 * (eKmean**2) / (2*(eKmean**2) - 3*config.nParticles*(eKvar)  )
-    
-def errorBootstrap(data, numSamples):
+
+def errorBootstrap(sections, numSamples, function):
     #initialize samples to zero
     samples=np.zeros(numSamples)
-    pointsPerSample = np.floor(len(data)/2)
+    sectionsPerSample = np.floor(len(sections)/2)
     for i in range(numSamples):
-        samples[i] = np.mean(np.random.choice(data,pointsPerSample,False));
+        samples[i] = function(sections[np.random.choice(sections.shape[0], sectionsPerSample, replace=False),:].flatten());
         
     s2 = np.var(samples);
-    return np.sqrt(s2/len(data))
+    return np.sqrt(s2/len(sections))
 
 def calcResult(pressure, eK, temp, eP, j):
     # Take pressure in stable region
@@ -37,21 +37,21 @@ def calcResult(pressure, eK, temp, eP, j):
     tempAvg=np.mean(tempStable)
     
     # Ititiate averages 
-    avgPressureSections=np.zeros(len(pressureStable)/config.oscLength[j]-1)
-    avgCvSections=np.zeros(len(eKStable)/config.oscLength[j]-1)
-    avgePSections=np.zeros(len(eKStable)/config.oscLength[j]-1)
-    avgTempSections=np.zeros(len(tempStable)/config.oscLength[j]-1)
+    pressureSections=np.zeros((len(pressureStable)/config.oscLength[j]-1, config.oscLength[j]-1))
+    cvSections=np.zeros((len(eKStable)/config.oscLength[j]-1, config.oscLength[j]-1))
+    ePSections=np.zeros((len(ePStable)/config.oscLength[j]-1, config.oscLength[j]-1))
+    tempSections=np.zeros((len(tempStable)/config.oscLength[j]-1, config.oscLength[j]-1))
     
     # Loop over all sections, calculate avg of parameters in sections
     for i in range( int( (config.iterations-config.stopRescaleIter) / config.oscLength[j] -1 ) ):
-        avgPressureSections[i]=np.mean(pressureStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
-        avgCvSections[i]=ComputeCv(eKStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
-        avgePSections[i]=np.mean(ePStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
-        avgTempSections[i]=np.mean(tempStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
+        pressureSections[i]=pressureStable[i*config.oscLength[j]:(i+1)*config.oscLength[j]-1]
+        cvSections[i]=eKStable[i*config.oscLength[j]:(i+1)*config.oscLength[j]-1]
+        ePSections[i]=ePStable[i*config.oscLength[j]:(i+1)*config.oscLength[j]-1]
+        tempSections[i]=tempStable[i*config.oscLength[j]:(i+1)*config.oscLength[j]-1]
     
-    pressureError = errorBootstrap(avgPressureSections,10);
-    cVError = errorBootstrap(avgCvSections,10);
-    ePError = errorBootstrap(avgePSections,10);
-    tempError = errorBootstrap(avgTempSections,10);
+    pressureError = errorBootstrap(pressureSections,50, np.mean);
+    cVError = errorBootstrap(cvSections,50, ComputeCv);
+    ePError = errorBootstrap(ePSections,50, np.mean);
+    tempError = errorBootstrap(tempSections,50, np.mean);
         
     return pressureAvg, pressureError, cVAvg, cVError, tempAvg, tempError, ePAvg, ePError
