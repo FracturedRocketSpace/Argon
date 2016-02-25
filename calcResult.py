fbf283cf1095b5311f2eda176accce7dd6ac5618
Fabian <fool0003@hotmail.com>
2016-02-25 14:02:56 +0100

    Use stable part of the kinetic energy

--- calcResult.py
+++ calcResult.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb  4 09:54:44 2016
 
-@author: Mathijs
-"""
 import config
 import numpy as np
 
@@ -19,8 +14,9 @@ def calcResult(pressure, eK, temp, j):
         return 0,0,0,0;
         
     # Take pressure in stable region
-    pressureStable=pressure[config.stopRescaleIter::]
-    tempStable=temp[config.stopRescaleIter::]
+    pressureStable=pressure[config.stopRescaleIter::];
+    tempStable=temp[config.stopRescaleIter::];
+    eKStable=eK[config.stopRescaleIter::];
     
     # Compute Average values
     pressureAvg=np.mean(pressureStable)
@@ -36,7 +32,7 @@ def calcResult(pressure, eK, temp, j):
     # Loop over all sections, calculate avg of parameters in sections
     for i in range( int( (config.iterations-config.stopRescaleIter) / config.oscLength[j] -1 ) ):
         AvgPressure[i]=np.mean(pressureStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
-        cvAvgSections[i]=ComputeCv(eK[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
+        cvAvgSections[i]=ComputeCv(eKStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
         AvgTemp[i]=np.mean(tempStable[i*config.oscLength[j]:i*config.oscLength[j]+config.oscLength[j]-1])
 
     # Compute error
